756662f6c6bfa7a3413e0d617d4fd1efa9ec842c
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 17:24:43 +0200

    Ajout du titre en variable.

--- application/modules/pcet/views/loireatlantique/index.php
+++ application/modules/pcet/views/loireatlantique/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>PCET en Loire-Atlantique</h3>
+	<h3><?php echo lang('pcet'); ?> en Loire-Atlantique</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
