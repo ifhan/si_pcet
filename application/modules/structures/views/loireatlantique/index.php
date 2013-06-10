ca2b7475692e944da2a2db4401f2e0cba02ff5e7
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 15:54:57 +0200

    Ajout d'une variable pour le titre.

--- application/modules/structures/views/loireatlantique/index.php
+++ application/modules/structures/views/loireatlantique/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>Liste des collectivit&eacute;s porteuses de PCET en Loire-Atlantique</h3>
+	<h3><?php echo lang('structures_porteuses'); ?>Loire-Atlantique</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
