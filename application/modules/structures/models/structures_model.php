080698bb723ef00223be1b535b6faf7abd52c786
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 16:02:55 +0200

    Mise en forme.

--- application/modules/structures/models/structures_model.php
+++ application/modules/structures/models/structures_model.php
@@ -2,29 +2,29 @@
 
 class Structures_model extends BF_Model {
 
-	protected $table		= "pcet_structure";
-	protected $key			= "id";
+	protected $table	= "pcet_structure";
+	protected $key		= "id";
 	protected $soft_deletes	= false;
 	protected $date_format	= "datetime";
 	protected $set_created	= false;
 	protected $set_modified = false;
 
 	function get_structures_by_departement($departement) {
-		$records = $this->structures_model
-			->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
-			->select('pcet_structure.id as id, pcet_structure.ID_STR as ID_STR, pcet_type_str.NOM_TYPE as NOM_TYPE')
-			->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
-			->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
-			->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
-			->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
-			->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
-			->select('bdc_commune_52.Nom_Commune as Nom_Commune')
-			->select('bdc_departement_52.Nom_Departement as Nom_Departement')
-			->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
-			->select('r_pays_contour_r52.nom as nom_pays')
-			->select('r_pnr_r52.nom as nom_pnr')
-			->find_all_by('DEPARTEMENT_id',$departement);
-		return $records;
+            $records = $this->structures_model
+		->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
+		->select('pcet_structure.id as id, pcet_structure.ID_STR as ID_STR, pcet_type_str.NOM_TYPE as NOM_TYPE')
+		->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
+		->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
+		->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
+		->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
+		->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
+		->select('bdc_commune_52.Nom_Commune as Nom_Commune')
+		->select('bdc_departement_52.Nom_Departement as Nom_Departement')
+		->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
+		->select('r_pays_contour_r52.nom as nom_pays')
+		->select('r_pnr_r52.nom as nom_pnr')
+		->find_all_by('DEPARTEMENT_id',$departement);
+            return $records;
 	}
 	
 	function list_structures_by_departement($departement) {
