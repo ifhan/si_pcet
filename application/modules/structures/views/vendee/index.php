14ae2b78199020bd8f52ba36add965fe12554057
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 15:55:46 +0200

    Ajout d'une variable pour le titre.

--- application/modules/structures/views/vendee/index.php
+++ application/modules/structures/views/vendee/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>Liste des collectivit&eacute;s porteuses de PCET en Vend&eacute;e</h3>
+	<h3><?php echo lang('structures_porteuses'); ?>Vend&eacute;e</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
