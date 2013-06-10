da40eefee5e010df976cbfe5ec8d868caa4ffc94
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 14:59:05 +0200

    Modification de la fonction list_structures_by_departement($departement)

--- application/modules/structures/models/structures_model.php
+++ application/modules/structures/models/structures_model.php
@@ -27,27 +27,26 @@ class Structures_model extends BF_Model {
 		return $records;
 	}
 	
-	function list_structures_by_departement($departement)
-    {
-        $structures = $this->db
-			->select('pcet_structure.id as id, pcet_structure.ID_STR as ID_STR,
-				pcet_structure.TYPE_STRUCTURE_id as TYPE_STRUCTURE_id,
-				pcet_structure.DEPARTEMENT_id as DEPARTEMENT_id')
-			->where('pcet_structure.DEPARTEMENT_id',$departement)
-			->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
-			->select('pcet_type_str.NOM_TYPE as NOM_TYPE')
-			->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
-			->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
-			->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
-			->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
-			->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
-			->select('bdc_commune_52.Nom_Commune as Nom_Commune')
-			->select('bdc_departement_52.Nom_Departement as Nom_Departement')
-			->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
-			->select('r_pays_contour_r52.nom as nom_pays')
-			->select('r_pnr_r52.nom as nom_pnr')
-			->order_by('pcet_structure.ID_STR', 'asc');
-        $structures = $this->db->get('pcet_structure');
+	function list_structures_by_departement($departement) {
+            $structures = $this->db
+                 ->select('pcet_structure.id as id, pcet_structure.ID_STR as ID_STR,
+                    pcet_structure.TYPE_STRUCTURE_id as TYPE_STRUCTURE_id,
+                    pcet_structure.DEPARTEMENT_id as DEPARTEMENT_id')
+                 ->where('pcet_structure.DEPARTEMENT_id',$departement)
+                 ->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
+                 ->select('pcet_type_str.NOM_TYPE as NOM_TYPE')
+                 ->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
+                 ->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
+                 ->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
+                 ->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
+                 ->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
+                 ->select('bdc_commune_52.Nom_Commune as Nom_Commune')
+                 ->select('bdc_departement_52.Nom_Departement as Nom_Departement')
+                 ->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
+                 ->select('r_pays_contour_r52.nom as nom_pays')
+                 ->select('r_pnr_r52.nom as nom_pnr')
+                 ->order_by('pcet_structure.ID_STR', 'asc');
+            $structures = $this->db->get('pcet_structure');
 
         foreach ($structures->result() as $structure)
         {
