b2050c47e5f5050dd58593519b0d4aabd130c6e2
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-10 16:37:19 +0200

    Ajout de nouvelles variables de langage.

---
+++ application/modules/adaptation/language/french/adaptation_lang.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['adaptation']			= 'Études de vulnérabilité';
+$lang['adaptation_manage']			= 'Gérer les études de vulnérabilité';
+$lang['adaptation_add']	= 'Études de vulnérabilité pour les PCET de ';
+$lang['adaptation_add']				= 'Ajouter une étude de vulnérabilité';
+$lang['adaptation_edit']				= 'Éditer une étude de vulnérabilité';
+$lang['adaptation_true']				= 'Vrai';
+$lang['adaptation_false']				= 'Faux';
+$lang['adaptation_create']			= 'Create';
+$lang['adaptation_list']				= 'Liste';
+$lang['adaptation_new']				= 'Nouvelle';
+$lang['adaptation_edit_text']			= 'Edit this to suit your needs';
+$lang['adaptation_no_records']			= 'There aren\'t any adaptation in the system.';
+$lang['adaptation_create_new']			= 'Create a new Adaptation.';
+$lang['adaptation_create_success']			= 'Adaptation successfully created.';
+$lang['adaptation_create_failure']			= 'There was a problem creating the adaptation: ';
+$lang['adaptation_create_new_button']			= 'Create New Adaptation';
+$lang['adaptation_invalid_id']			= 'Invalid Adaptation ID.';
+$lang['adaptation_edit_success']			= 'Adaptation successfully saved.';
+$lang['adaptation_edit_failure']			= 'There was a problem saving the adaptation: ';
+$lang['adaptation_delete_success']			= 'record(s) successfully deleted.';
+$lang['adaptation_delete_failure']			= 'We could not delete the record: ';
+$lang['adaptation_delete_error']			= 'You have not selected any records to delete.';
+$lang['adaptation_actions']			= 'Actions';
+$lang['adaptation_cancel']			= 'Cancel';
+$lang['adaptation_delete_record']			= 'Delete this Adaptation';
+$lang['adaptation_delete_confirm']			= 'Are you sure you want to delete this adaptation?';
+$lang['adaptation_edit_heading']			= 'Edit Adaptation';
+
+// Create/Edit Buttons
+$lang['adaptation_action_edit']				= 'Save Adaptation';
+$lang['adaptation_action_create']				= 'Create Adaptation';
+
+// Activities
+$lang['adaptation_act_create_record']			= 'Created record with ID';
+$lang['adaptation_act_edit_record']			= 'Updated record with ID';
+$lang['adaptation_act_delete_record']			= 'Deleted record with ID';
