1fe38d899ae76181810b953d2e72f39691b70b11
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-11 09:30:36 +0200

    Mise en forme + ajout d'une variable langage + modification du tableau.

--- application/modules/pcet/views/vendee/index.php
+++ application/modules/pcet/views/vendee/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>PCET</h3>
+	<h3><?php echo lang('pcet'); ?> en Vendée</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
@@ -8,30 +8,25 @@
 					<th class="column-check"><input class="check-all" type="checkbox" /></th>
 					<?php endif;?>
 					
-					<th>Statut</th>
 					<th>Identifiant du PCET</th>
-					<th>Commentaire sur l etat du PCET</th>
+					<th>Type</th>
+					<th>Collectivit&eacute;</th>
+					<th>Statut</th>
+					<th>&Eacute;tat d'avancement</th>
+					<th>Commentaires</th>
 					<th>Site web</th>
-					<th>CONTRAT ADEME PCET</th>
-					<th>TYPE CONTRAT ADEME PCET</th>
-					<th>Type de structure</th>
-					<th>ID AVIS</th>
-					<th>ID ENGAGE</th>
-					<th>ID INDIC</th>
-					<th>ID DIAG</th>
-					<th>Phase</th>
-					<th>ID ADAPT</th>
-					<th>ID GOUV</th>
+					<th>Contrat ADEME</th>
+					<th>Type de contrat</th>
 				</tr>
 			</thead>
 			<?php if (isset($records) && is_array($records) && count($records)) : ?>
 			<tfoot>
 				<?php if ($this->auth->has_permission('PCET.Vendee.Delete')) : ?>
 				<tr>
-					<td colspan="15">
-						<?php echo lang('bf_with_selected') ?>
+					<td colspan="9">
 						<input type="submit" name="delete" id="delete-me" class="btn btn-danger" value="<?php echo lang('bf_action_delete') ?>" onclick="return confirm('<?php e(js_escape(lang('pcet_delete_confirm'))); ?>')">
-					</td>
+                                                <?php echo lang('bf_with_selected') ?>
+                                        </td>
 				</tr>
 				<?php endif;?>
 			</tfoot>
@@ -44,30 +39,36 @@
 					<td><input type="checkbox" name="checked[]" value="<?php echo $record->id ?>" /></td>
 					<?php endif;?>
 					
-				<?php if ($this->auth->has_permission('PCET.Vendee.Edit')) : ?>
-				<td><?php echo anchor(SITE_AREA .'/vendee/pcet/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->STATUT_PCET) ?></td>
-				<?php else: ?>
-				<td><?php e($record->STATUT_PCET) ?></td>
-				<?php endif; ?>
-			
-				<td><?php e($record->ID_PCET) ?></td>
-				<td><?php e($record->ETAT_PCET) ?></td>
-				<td><?php e($record->INTERNET_PCET) ?></td>
-				<td><?php e($record->CONTRAT_ADEME_PCET) ?></td>
-				<td><?php e($record->TYPE_CONTRAT_ADEME_PCET) ?></td>
-				<td><?php e($record->ID_STR) ?></td>
-				<td><?php e($record->ID_AVIS) ?></td>
-				<td><?php e($record->ID_ENGAGE) ?></td>
-				<td><?php e($record->ID_INDIC) ?></td>
-				<td><?php e($record->ID_DIAG) ?></td>
-				<td><?php e($record->ID_PHASE) ?></td>
-				<td><?php e($record->ID_ADAPT) ?></td>
-				<td><?php e($record->ID_GOUV) ?></td>
+					<?php if ($this->auth->has_permission('PCET.Vendee.Edit')) : ?>
+					<td><?php echo anchor(SITE_AREA .'/vendee/pcet/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->ID_PCET) ?></td>
+					<?php else: ?>
+					<td><?php e($record->ID_PCET) ?></td>
+					<?php endif; ?>
+				
+					<td><?php e($record->NOM_TYPE) ?></td>
+					<td><?php e($record->Nom_Commune) ?><?php e($record->Nom_Departement) ?><?php e($record->NOM_EPCI) ?><?php e($record->nom_pays) ?><?php e($record->nom_pnr) ?></td>
+					<td><?php e($record->STATUT_PCET) ?></td>
+					<td><?php e($record->NOM_PHASE) ?></td>
+					<td><?php e($record->ETAT_PCET) ?></td>
+					<td>
+						<?php if(!empty($record->INTERNET_PCET)): ?>
+							<?php echo anchor($record->INTERNET_PCET, 'Consulter', 'target="_blank"'); ?>
+						<?php endif; ?>
+					</td>
+					<td>
+						<?php if($record->CONTRAT_ADEME_PCET == '1'): ?>
+							<?php echo "Oui"; ?>
+						<?php else: ?>
+							<?php echo "Non"; ?>
+						<?php endif; ?>
+					</td>
+					<td><?php e($record->TYPE_CONTRAT_ADEME_PCET) ?></td>
+
 				</tr>
 			<?php endforeach; ?>
 			<?php else: ?>
 				<tr>
-					<td colspan="15">No records found that match your selection.</td>
+					<td colspan="9"><?php echo lang('bf_no_record_found'); ?></td>
 				</tr>
 			<?php endif; ?>
 			</tbody>
