68b031b8520d346b6d007d38eae93d5063fcf30e
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-12 12:24:18 +0200

    Modification du tableau.

--- application/modules/pcet/views/index.php
+++ application/modules/pcet/views/index.php
@@ -1,5 +1,5 @@
 <div>
-	<h1 class="page-header">PCET</h1>
+	<h1 class="page-header">PCET en Pays de la Loire</h1>
 </div>
 
 <br />
@@ -9,35 +9,29 @@
 	<table class="table table-striped table-bordered">
 		<thead>
 			<tr>
-				<th>Collectivit&eacute;</th>
-				<th>Identifiant du PCET</th>
-				<th>Statut</th>
-				<th>&Eacute;tat d'avancement</th>
-				<th>Commentaires</th>
-				<th>Site web</th>
-				<th>Contrat ADEME</th>
-				<th>Type de contrat ADEME</th>
+                            <th>Identifiant du PCET</th>
+                            <th>Type</th>
+                            <th>Collectivit&eacute;</th>
+                            <th>Statut</th>
+                            <th>État d'avancement</th>
+                            <th>Site web</th>
 			</tr>
 		</thead>
 		<tbody>
 		
 		<?php foreach ($records as $record) : ?>
-			<?php $record = (array)$record;?>
+			
 			<tr>
-			<?php foreach($record as $field => $value) : ?>
-				
-				<?php if ($field != 'id') : ?>
-					<td>
-						<?php if ($field == 'deleted'): ?>
-							<?php e(($value > 0) ? lang('pcet_true') : lang('pcet_false')); ?>
-						<?php else: ?>
-							<?php e($value); ?>
-						<?php endif ?>
-					</td>
+                            <td><?php echo anchor('/pcet/show/'. $record->id, '<i class="icon-search">&nbsp;</i>' .  $record->ID_PCET) ?>
+                            <td><?php e($record->NOM_TYPE) ?></td>
+                            <td><?php e($record->Nom_Commune) ?><?php e($record->Nom_Departement) ?><?php e($record->NOM_EPCI) ?><?php e($record->nom_pays) ?><?php e($record->nom_pnr) ?></td>
+                            <td><?php e($record->STATUT_PCET) ?></td>
+                            <td><?php e($record->NOM_PHASE) ?></td>
+                            <td width="10%">
+                                <?php if(!empty($record->INTERNET_PCET)): ?>
+                                    <?php echo anchor($record->INTERNET_PCET, '<i class="icon-globe">&nbsp;</i>Consulter', 'target="_blank"'); ?>
 				<?php endif; ?>
-				
-			<?php endforeach; ?>
-
+                            </td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
