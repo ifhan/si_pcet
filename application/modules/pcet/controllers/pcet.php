502b6c1ba3945c94c6fe6abd79560e0924bc6763
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-12 16:53:20 +0200

    Modification de la variable de paramétrage des fonctions.

--- application/modules/pcet/controllers/pcet.php
+++ application/modules/pcet/controllers/pcet.php
@@ -12,10 +12,11 @@ class pcet extends Front_Controller {
 		$this->load->library('form_validation');
 		$this->load->model('pcet_model', null, true);
 		$this->load->model('structures/structures_model', null, true);
-		$this->load->model('phases/phases_model', null, true);                
+		$this->load->model('phases/phases_model', null, true);
+                $this->load->model('adaptation/adaptation_model', null, true);
                 $this->load->helper('typography');
 		$this->lang->load('pcet');
-		
+                	
 	}
 
 	//--------------------------------------------------------------------
@@ -44,16 +45,19 @@ class pcet extends Front_Controller {
 
 		Displays a record of form data from several tables.
 	*/
-	public function show($id)
+	public function show($ID_PCET)
 	{
 
-            $structure = $this->structures_model->get_structure_by_id($id);
-            $phase = $this->phases_model->get_phase_by_id($id);
-            $pcet = $this->pcet_model->find_by('id',$id);
+            $structure = $this->structures_model->get_structure_by_id($ID_PCET);
+            $phase = $this->phases_model->get_phase_by_id($ID_PCET);
+            $pcet = $this->pcet_model->find_by('ID_PCET',$ID_PCET);
+            $adaptation = $this->adaptation_model->find_by('ID_PCET',$ID_PCET);
 		
             Template::set('pcet', $pcet);
             Template::set('structure', $structure);
             Template::set('phase', $phase);
+            Template::set('adaptation', $adaptation);
+            Template::set('toolbar_title', lang('pcet_list'));
             Template::render();
 	}
 
