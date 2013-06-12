61904b0ab6779a2e36a790ac2f47986ecd620d6a
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-12 14:33:14 +0200

    Ajout d'une variable de langage dans le titre + typo.

--- application/modules/adaptation/views/loireatlantique/edit.php
+++ application/modules/adaptation/views/loireatlantique/edit.php
@@ -20,7 +20,7 @@ $id = isset($adaptation['id']) ? $adaptation['id'] : '';
 
 ?>
 <div class="admin-box">
-	<h3>Adaptation</h3>
+	<h3><?php echo lang('adaptation_edit'); ?></h3>
 	<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
 		<fieldset>
 
@@ -39,7 +39,7 @@ $id = isset($adaptation['id']) ? $adaptation['id'] : '';
 			</div>
 
 			<div class="control-group <?php echo form_error('METHODE_ADAPT') ? 'error' : ''; ?>">
-				<?php echo form_label('Methodes employees', 'adaptation_METHODE_ADAPT', array('class' => 'control-label') ); ?>
+				<?php echo form_label('Méthodes employées', 'adaptation_METHODE_ADAPT', array('class' => 'control-label') ); ?>
 				<div class='controls'>
 					<?php echo form_textarea( array( 'name' => 'adaptation_METHODE_ADAPT', 'id' => 'adaptation_METHODE_ADAPT', 'rows' => '5', 'cols' => '80', 'value' => set_value('adaptation_METHODE_ADAPT', isset($adaptation['METHODE_ADAPT']) ? $adaptation['METHODE_ADAPT'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('METHODE_ADAPT'); ?></span>
@@ -47,7 +47,7 @@ $id = isset($adaptation['id']) ? $adaptation['id'] : '';
 			</div>
 
 			<div class="control-group <?php echo form_error('ALEA_ADAPT') ? 'error' : ''; ?>">
-				<?php echo form_label('Aleas identifies', 'adaptation_ALEA_ADAPT', array('class' => 'control-label') ); ?>
+				<?php echo form_label('Aléas identifiés', 'adaptation_ALEA_ADAPT', array('class' => 'control-label') ); ?>
 				<div class='controls'>
 					<?php echo form_textarea( array( 'name' => 'adaptation_ALEA_ADAPT', 'id' => 'adaptation_ALEA_ADAPT', 'rows' => '5', 'cols' => '80', 'value' => set_value('adaptation_ALEA_ADAPT', isset($adaptation['ALEA_ADAPT']) ? $adaptation['ALEA_ADAPT'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('ALEA_ADAPT'); ?></span>
@@ -56,10 +56,10 @@ $id = isset($adaptation['id']) ? $adaptation['id'] : '';
 
 			<div class="form-actions">
 				<input type="submit" name="save" class="btn btn-primary" value="<?php echo lang('adaptation_action_edit'); ?>"  />
-				or <?php echo anchor(SITE_AREA .'/loireatlantique/adaptation', lang('adaptation_cancel'), 'class="btn btn-warning"'); ?>
+				ou <?php echo anchor(SITE_AREA .'/loireatlantique/adaptation', lang('adaptation_cancel'), 'class="btn btn-warning"'); ?>
 				
 			<?php if ($this->auth->has_permission('Adaptation.Loireatlantique.Delete')) : ?>
-				or
+				ou
 				<button type="submit" name="delete" class="btn btn-danger" id="delete-me" onclick="return confirm('<?php e(js_escape(lang('adaptation_delete_confirm'))); ?>'); ">
 					<span class="icon-trash icon-white"></span>&nbsp;<?php echo lang('adaptation_delete_record'); ?>
 				</button>
