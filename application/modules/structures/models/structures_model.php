f8b5d19b2e195fb1959e7cc1ea12a32b84f1c644
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-12 16:51:37 +0200

    Modification de la variable de paramétrage.

--- application/modules/structures/models/structures_model.php
+++ application/modules/structures/models/structures_model.php
@@ -57,12 +57,12 @@ class Structures_model extends BF_Model {
             return $structures;
         }
         
-        function get_structure_by_id($id) {
+        function get_structure_by_id($ID_PCET) {
             
             $structure = $this->structures_model
                     ->join('pcet','pcet_structure.ID_STR = pcet.ID_STR','left')
                     ->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
-                    ->select('pcet_structure.ID_STR as ID_STR, pcet_structure.DEPARTEMENT_id as DEPARTEMENT_id, pcet_type_str.NOM_TYPE as NOM_TYPE')
+                    ->select('pcet.ID_PCET as ID_PCET, pcet_structure.ID_STR as ID_STR, pcet_structure.DEPARTEMENT_id as DEPARTEMENT_id, pcet_type_str.NOM_TYPE as NOM_TYPE')
                     ->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
                     ->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
                     ->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
@@ -73,7 +73,7 @@ class Structures_model extends BF_Model {
                     ->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
                     ->select('r_pays_contour_r52.nom as nom_pays')
                     ->select('r_pnr_r52.nom as nom_pnr')
-                    ->find_by('pcet.id',$id);
+                    ->find_by('ID_PCET',$ID_PCET);
             return $structure;
         }
     
