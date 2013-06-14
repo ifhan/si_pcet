eb276270e15d29567aa46189de50b06a6f44d76f
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-14 10:03:00 +0200

    Ajout de variables de langage.

--- application/modules/engagement/language/french/engagement_lang.php
+++ application/modules/engagement/language/french/engagement_lang.php
@@ -9,24 +9,24 @@ $lang['engagement_new']				= 'Nouveau';
 $lang['engagement_edit_text']			= 'Edit this to suit your needs';
 $lang['engagement_no_records']			= 'There aren\'t any engagement in the system.';
 $lang['engagement_create_new']			= 'Create a new Engagement.';
-$lang['engagement_create_success']			= 'Engagement successfully created.';
+$lang['engagement_create_success']			= 'Enregistrement créé avec succès.';
 $lang['engagement_create_failure']			= 'There was a problem creating the engagement: ';
 $lang['engagement_create_new_button']			= 'Create New Engagement';
 $lang['engagement_invalid_id']			= 'Invalid Engagement ID.';
-$lang['engagement_edit_success']			= 'Engagement successfully saved.';
+$lang['engagement_edit_success']			= 'Enregistrement sauvegardé avec succès.';
 $lang['engagement_edit_failure']			= 'There was a problem saving the engagement: ';
-$lang['engagement_delete_success']			= 'record(s) successfully deleted.';
+$lang['engagement_delete_success']			= 'enregistrement(s) supprimés avec succès.';
 $lang['engagement_delete_failure']			= 'We could not delete the record: ';
 $lang['engagement_delete_error']			= 'You have not selected any records to delete.';
 $lang['engagement_actions']			= 'Actions';
-$lang['engagement_cancel']			= 'Cancel';
-$lang['engagement_delete_record']			= 'Delete this Engagement';
+$lang['engagement_cancel']			= 'Annuler';
+$lang['engagement_delete_record']			= 'Supprimer';
 $lang['engagement_delete_confirm']			= 'Are you sure you want to delete this engagement?';
 $lang['engagement_edit_heading']			= 'Edit Engagement';
 
 // Create/Edit Buttons
-$lang['engagement_action_edit']				= 'Save Engagement';
-$lang['engagement_action_create']				= 'Create Engagement';
+$lang['engagement_action_edit']				= 'Enregistrer';
+$lang['engagement_action_create']				= 'Enregistrer';
 
 // Activities
 $lang['engagement_act_create_record']			= 'Created record with ID';
