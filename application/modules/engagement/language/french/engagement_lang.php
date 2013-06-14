1f3d18785737a420d77746cfd3450e5afb2374cb
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-14 12:05:46 +0200

    Ajout de variables de langage.

--- application/modules/engagement/language/french/engagement_lang.php
+++ application/modules/engagement/language/french/engagement_lang.php
@@ -1,20 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$lang['engagement_manage']			= 'Suivre l\'engagement';
-$lang['engagement_edit']				= 'Edit';
+$lang['engagement']			= 'Engagement de la démarche';
+$lang['engagement_title']			= 'Engagement des démarches en ';
+$lang['engagement_manage']			= 'Suivre l\'engagement des démarches';
+$lang['engagement_edit']				= 'Modifier le suivi de l\'engagement de la démarche';
 $lang['engagement_true']				= 'True';
 $lang['engagement_false']				= 'False';
-$lang['engagement_create']			= 'Créer';
+$lang['engagement_create']			= 'Suivre l\'engagement de la démarche';
 $lang['engagement_list']				= 'Liste';
 $lang['engagement_new']				= 'Nouveau';
 $lang['engagement_edit_text']			= 'Edit this to suit your needs';
 $lang['engagement_no_records']			= 'There aren\'t any engagement in the system.';
-$lang['engagement_create_new']			= 'Create a new Engagement.';
+$lang['engagement_create_new']			= 'Créer un nouvel enregistrement.';
 $lang['engagement_create_success']			= 'Enregistrement créé avec succès.';
-$lang['engagement_create_failure']			= 'There was a problem creating the engagement: ';
+$lang['engagement_create_failure']			= 'Il y a eu un problème avec la création de cet enregistrement: ';
 $lang['engagement_create_new_button']			= 'Create New Engagement';
 $lang['engagement_invalid_id']			= 'Invalid Engagement ID.';
 $lang['engagement_edit_success']			= 'Enregistrement sauvegardé avec succès.';
-$lang['engagement_edit_failure']			= 'There was a problem saving the engagement: ';
+$lang['engagement_edit_failure']			= 'Il y a eu un problème avec la sauvegarde de cet enregistrement: ';
 $lang['engagement_delete_success']			= 'enregistrement(s) supprimés avec succès.';
 $lang['engagement_delete_failure']			= 'We could not delete the record: ';
 $lang['engagement_delete_error']			= 'You have not selected any records to delete.';
@@ -22,7 +24,7 @@ $lang['engagement_actions']			= 'Actions';
 $lang['engagement_cancel']			= 'Annuler';
 $lang['engagement_delete_record']			= 'Supprimer';
 $lang['engagement_delete_confirm']			= 'Are you sure you want to delete this engagement?';
-$lang['engagement_edit_heading']			= 'Edit Engagement';
+$lang['engagement_edit_heading']			= 'Éditer l\'enregistrement';
 
 // Create/Edit Buttons
 $lang['engagement_action_edit']				= 'Enregistrer';
