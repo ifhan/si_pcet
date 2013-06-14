850c539431fde07aa97f2e219362c2d7d2334c15
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-14 16:34:10 +0200

    Ajout de variables de langage.

--- application/modules/avis/language/french/avis_lang.php
+++ application/modules/avis/language/french/avis_lang.php
@@ -1,32 +1,33 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$lang['avis_manage']			= 'Manage Avis';
-$lang['avis_edit']				= 'Edit';
-$lang['avis_true']				= 'True';
-$lang['avis_false']				= 'False';
+$lang['avis']                    = 'Avis';
+$lang['avis_manage']			= 'Suivre les avis';
+$lang['avis_edit']				= 'Éditer';
+$lang['avis_true']				= 'Vrai';
+$lang['avis_false']				= 'Faux';
 $lang['avis_create']			= 'Create';
-$lang['avis_list']				= 'List';
-$lang['avis_new']				= 'New';
+$lang['avis_list']				= 'Liste';
+$lang['avis_new']				= 'Nouveau';
 $lang['avis_edit_text']			= 'Edit this to suit your needs';
 $lang['avis_no_records']			= 'There aren\'t any avis in the system.';
 $lang['avis_create_new']			= 'Create a new Avis.';
-$lang['avis_create_success']			= 'Avis successfully created.';
+$lang['avis_create_success']			= 'Création effectuée avec succès.';
 $lang['avis_create_failure']			= 'There was a problem creating the avis: ';
 $lang['avis_create_new_button']			= 'Create New Avis';
 $lang['avis_invalid_id']			= 'Invalid Avis ID.';
-$lang['avis_edit_success']			= 'Avis successfully saved.';
+$lang['avis_edit_success']			= 'Modification effectuée avec succès.';
 $lang['avis_edit_failure']			= 'There was a problem saving the avis: ';
-$lang['avis_delete_success']			= 'record(s) successfully deleted.';
+$lang['avis_delete_success']			= 'enregistrement(s) supprimés avec succès.';
 $lang['avis_delete_failure']			= 'We could not delete the record: ';
 $lang['avis_delete_error']			= 'You have not selected any records to delete.';
 $lang['avis_actions']			= 'Actions';
-$lang['avis_cancel']			= 'Cancel';
-$lang['avis_delete_record']			= 'Delete this Avis';
+$lang['avis_cancel']			= 'Annuler';
+$lang['avis_delete_record']			= 'Supprimer';
 $lang['avis_delete_confirm']			= 'Are you sure you want to delete this avis?';
 $lang['avis_edit_heading']			= 'Edit Avis';
 
 // Create/Edit Buttons
-$lang['avis_action_edit']				= 'Save Avis';
-$lang['avis_action_create']				= 'Create Avis';
+$lang['avis_action_edit']				= 'Enregistrer';
+$lang['avis_action_create']				= 'Enregistrer';
 
 // Activities
 $lang['avis_act_create_record']			= 'Created record with ID';
