9d51b1b3829fa112acc73321da7f29eec3b69419
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-14 12:02:38 +0200

    Ajout d'une fonction get_engagement_by_departement($departement).

--- application/modules/engagement/models/engagement_model.php
+++ application/modules/engagement/models/engagement_model.php
@@ -8,4 +8,32 @@ class Engagement_model extends BF_Model {
 	protected $date_format	= "datetime";
 	protected $set_created	= false;
 	protected $set_modified = false;
+        
+        function get_engagement_by_departement($departement) {
+            
+            $records = $this->engagement_model
+                ->join('pcet','pcet.ID_PCET = pcet_engagement.ID_PCET','left')
+                ->join('pcet_structure','pcet_structure.ID_STR = pcet.ID_STR','left')
+                ->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
+                ->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
+                ->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
+                ->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
+                ->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
+                ->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
+                ->select('pcet_type_str.NOM_TYPE as NOM_TYPE')
+                ->select('bdc_commune_52.Nom_Commune as Nom_Commune')
+                ->select('bdc_departement_52.Nom_Departement as Nom_Departement')
+                ->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
+                ->select('r_pays_contour_r52.nom as nom_pays')
+                ->select('r_pnr_r52.nom as nom_pnr')
+                ->select('pcet_engagement.id as id, pcet_engagement.ID_PCET as ID_PCET, pcet_engagement.DATE_DELIB as DATE_DELIB,
+                    pcet_engagement.NOTIF_DELIB_ETAT as NOTIF_DELIB_ETAT, pcet_engagement.NOTIF_DELIB_CR as NOTIF_DELIB_CR, 
+                    pcet_engagement.NOTIF_USH as NOTIF_USH, pcet_engagement.REP_USH as REP_USH, pcet_engagement.DATE_REP_USH as DATE_REP_USH,
+                    pcet_engagement.DATE_PAC as DATE_PAC')
+                ->order_by('pcet.ID_PCET','asc')
+                ->find_all_by('pcet_structure.DEPARTEMENT_id',$departement);
+            
+            return $records;
+                        
+        }
 }
