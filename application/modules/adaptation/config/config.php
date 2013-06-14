a818aaa0f5030a5ed9cabdc2038d95348eec89d3
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-14 08:34:56 +0200

    Modification de l'alias du module.

--- application/modules/adaptation/config/config.php
+++ application/modules/adaptation/config/config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');$config['module_config'] = array(
 	'description'	=> 'Suivi des études de vulnérabilité liées au PCET',
-	'name'		=> 'Études de vulnérabilité',
+	'name'		=> 'Volet Adaptation',
 	'version'		=> '0.0.2',
 	'author'		=> 'ronan.vignard'
 );
