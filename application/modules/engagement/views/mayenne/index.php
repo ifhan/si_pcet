3649445832b9241e65bdc4a09f3c064592b587fe
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-17 15:46:58 +0200

    Mise à jour des vues  pour les départements restants.

--- application/modules/engagement/views/mayenne/index.php
+++ application/modules/engagement/views/mayenne/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>Engagement</h3>
+	<h3><?php echo lang('engagement_title'); ?>Mayenne</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
@@ -8,25 +8,24 @@
 					<th class="column-check"><input class="check-all" type="checkbox" /></th>
 					<?php endif;?>
 					
-					<th>Identifiant du PCET</th>
-					<th>Commentaire sur l implication de la ddt sur le projet</th>
-					<th>Date de deliberation</th>
-					<th>Date du courrier de la structure notifiant sa deliberation a l Etat</th>
-					<th>Date du courrier de la structure notifiant sa deliberation au Conseil regional</th>
-					<th>Date courrier de notification d engagement de la collectivite a l USH</th>
-					<th>Consultation aval USH souhaitee</th>
-					<th>Date du courrier de reponse de l USH</th>
-					<th>Date du Porter-a-connaissance de l Etat</th>
+					<th>PCET</th>
+					<th>Date de déliberation</th>
+					<th>Date du courrier de la collectivité notifiant sa déliberation a l'État</th>
+					<th>Date du courrier de la collectivité notifiant sa déliberation au Conseil regional</th>
+					<th>Date courrier de notification d'engagement de la collectivité à l'USH</th>
+					<th>Consultation aval de l'USH souhaitée ?</th>
+					<th>Date du courrier de réponse de l'USH</th>
+					<th>Date du Porter-à-connaissance de l'État</th>
 				</tr>
 			</thead>
 			<?php if (isset($records) && is_array($records) && count($records)) : ?>
 			<tfoot>
 				<?php if ($this->auth->has_permission('Engagement.Mayenne.Delete')) : ?>
 				<tr>
-					<td colspan="10">
-						<?php echo lang('bf_with_selected') ?>
+					<td colspan="9">
 						<input type="submit" name="delete" id="delete-me" class="btn btn-danger" value="<?php echo lang('bf_action_delete') ?>" onclick="return confirm('<?php e(js_escape(lang('engagement_delete_confirm'))); ?>')">
-					</td>
+                                                <?php echo lang('bf_with_selected') ?>
+                                        </td>
 				</tr>
 				<?php endif;?>
 			</tfoot>
@@ -40,24 +39,51 @@
 					<?php endif;?>
 					
 				<?php if ($this->auth->has_permission('Engagement.Mayenne.Edit')) : ?>
-				<td><?php echo anchor(SITE_AREA .'/mayenne/engagement/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->ID_PCET) ?></td>
+				<td><?php echo anchor(SITE_AREA .'/mayenne/engagement/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->ID_PCET.' - '.$record->NOM_TYPE.' - '.$record->Nom_Commune.$record->Nom_Departement.$record->NOM_EPCI.$record->nom_pays.$record->nom_pnr) ?></td>
 				<?php else: ?>
-				<td><?php e($record->ID_PCET) ?></td>
+				<td><?php e($record->ID_PCET.' - '.$record->NOM_TYPE.' - '.$record->Nom_Commune.$record->Nom_Departement.$record->NOM_EPCI.$record->nom_pays.$record->nom_pnr) ?></td>
 				<?php endif; ?>
-			
-				<td><?php e($record->COMMENT_DDT) ?></td>
-				<td><?php e($record->DATE_DELIB) ?></td>
-				<td><?php e($record->NOTIF_DELIB_ETAT) ?></td>
-				<td><?php e($record->NOTIF_DELIB_CR) ?></td>
-				<td><?php e($record->NOTIF_USH) ?></td>
-				<td><?php e($record->REP_USH) ?></td>
-				<td><?php e($record->DATE_REP_USH) ?></td>
-				<td><?php e($record->DATE_PAC) ?></td>
+                                
+				<td>
+                                    <?php if($record->DATE_DELIB !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->DATE_DELIB)))) ?>
+                                    <?php endif; ?>
+				<td>
+                                    <?php if($record->NOTIF_DELIB_ETAT !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_DELIB_ETAT)))) ?>
+                                    <?php endif; ?>
+                                </td>
+				<td>
+                                    <?php if($record->NOTIF_DELIB_CR !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_DELIB_CR)))) ?>
+                                    <?php endif; ?>
+                                </td>
+				<td>
+                                    <?php if($record->NOTIF_USH !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_USH)))) ?>
+                                    <?php endif; ?>
+                                </td>
+				<td>
+                                    <?php if($record->REP_USH == '1'): ?>
+                                        <?php echo "Oui"; ?>
+                                    <?php else: ?>
+                                        <?php echo "Non"; ?>
+                                    <?php endif; ?>
+				<td>
+                                    <?php if($record->DATE_REP_USH !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->DATE_REP_USH)))) ?>
+                                    <?php endif; ?>
+                                </td>
+				<td>
+                                    <?php if($record->DATE_PAC !== '0000-00-00'): ?>
+                                        <?php e(date("d/m/Y", strtotime(($record->DATE_PAC)))) ?>
+                                    <?php endif; ?>
+                                </td>
 				</tr>
 			<?php endforeach; ?>
 			<?php else: ?>
 				<tr>
-					<td colspan="10">No records found that match your selection.</td>
+					<td colspan="9"><?php echo lang('bf_no_record_found'); ?></td>
 				</tr>
 			<?php endif; ?>
 			</tbody>
