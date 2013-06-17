232987557df5b46eb35d4d28cab8b4a5c18a73f7
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-17 14:58:12 +0200

    Ajout d'une variable de langage.

--- application/modules/avis/views/loireatlantique/index.php
+++ application/modules/avis/views/loireatlantique/index.php
@@ -1,5 +1,5 @@
 <div class="admin-box">
-	<h3>Avis</h3>
+	<h3><?php echo lang('avis_titre')?>Loire-Atlantique</h3>
 	<?php echo form_open($this->uri->uri_string()); ?>
 		<table class="table table-striped">
 			<thead>
