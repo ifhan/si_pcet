2be9f5a313b16354096f34b9830f16d685fddb08
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-18 15:37:51 +0200

    Ajout d'un fichier de variables pour la langue française.

---
+++ application/modules/contacts/language/french/contacts_lang.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['contacts_manage']			= 'Manage Contacts';
+$lang['contacts_edit']				= 'Edit';
+$lang['contacts_true']				= 'True';
+$lang['contacts_false']				= 'False';
+$lang['contacts_create']			= 'Create';
+$lang['contacts_list']				= 'List';
+$lang['contacts_new']				= 'New';
+$lang['contacts_edit_text']			= 'Edit this to suit your needs';
+$lang['contacts_no_records']			= 'There aren\'t any contacts in the system.';
+$lang['contacts_create_new']			= 'Create a new Contacts.';
+$lang['contacts_create_success']			= 'Contacts successfully created.';
+$lang['contacts_create_failure']			= 'There was a problem creating the contacts: ';
+$lang['contacts_create_new_button']			= 'Create New Contacts';
+$lang['contacts_invalid_id']			= 'Invalid Contacts ID.';
+$lang['contacts_edit_success']			= 'Contacts successfully saved.';
+$lang['contacts_edit_failure']			= 'There was a problem saving the contacts: ';
+$lang['contacts_delete_success']			= 'record(s) successfully deleted.';
+$lang['contacts_delete_failure']			= 'We could not delete the record: ';
+$lang['contacts_delete_error']			= 'You have not selected any records to delete.';
+$lang['contacts_actions']			= 'Actions';
+$lang['contacts_cancel']			= 'Cancel';
+$lang['contacts_delete_record']			= 'Delete this Contacts';
+$lang['contacts_delete_confirm']			= 'Are you sure you want to delete this contacts?';
+$lang['contacts_edit_heading']			= 'Edit Contacts';
+
+// Create/Edit Buttons
+$lang['contacts_action_edit']				= 'Save Contacts';
+$lang['contacts_action_create']				= 'Create Contacts';
+
+// Activities
+$lang['contacts_act_create_record']			= 'Created record with ID';
+$lang['contacts_act_edit_record']			= 'Updated record with ID';
+$lang['contacts_act_delete_record']			= 'Deleted record with ID';
