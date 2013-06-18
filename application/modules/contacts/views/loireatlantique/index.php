d85982c5f2c3aa0b031c2b7c97f0799a219d9c0e
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-18 16:12:20 +0200

    Modification du tableau.

--- application/modules/contacts/views/loireatlantique/index.php
+++ application/modules/contacts/views/loireatlantique/index.php
@@ -8,21 +8,22 @@
 					<th class="column-check"><input class="check-all" type="checkbox" /></th>
 					<?php endif;?>
 					
-					<th>Civilite</th>
-					<th>Prenom</th>
+                                        <th>Collectivité</th>
+					<th>Civilité</th>
 					<th>Nom</th>
+                                        <th>Prénom</th>
 					<th>Courriel</th>
-					<th>Collectivite</th>
+                                        <th>Actions</th>
 				</tr>
 			</thead>
 			<?php if (isset($records) && is_array($records) && count($records)) : ?>
 			<tfoot>
 				<?php if ($this->auth->has_permission('Contacts.Loireatlantique.Delete')) : ?>
 				<tr>
-					<td colspan="6">
-						<?php echo lang('bf_with_selected') ?>
+					<td colspan="7">
 						<input type="submit" name="delete" id="delete-me" class="btn btn-danger" value="<?php echo lang('bf_action_delete') ?>" onclick="return confirm('<?php e(js_escape(lang('contacts_delete_confirm'))); ?>')">
-					</td>
+                                                <?php echo lang('bf_with_selected') ?>
+                                        </td>
 				</tr>
 				<?php endif;?>
 			</tfoot>
@@ -35,21 +36,26 @@
 					<td><input type="checkbox" name="checked[]" value="<?php echo $record->id ?>" /></td>
 					<?php endif;?>
 					
-				<?php if ($this->auth->has_permission('Contacts.Loireatlantique.Edit')) : ?>
-				<td><?php echo anchor(SITE_AREA .'/loireatlantique/contacts/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->CIVILITE) ?></td>
-				<?php else: ?>
-				<td><?php e($record->CIVILITE) ?></td>
-				<?php endif; ?>
-			
+
+                                <td><?php e($record->NOM_TYPE) ?> - <?php e($record->Nom_Commune) ?><?php e($record->Nom_Departement) ?><?php e($record->NOM_EPCI) ?><?php e($record->nom_pays) ?><?php e($record->nom_pnr) ?></td>
+                                <td><?php e($record->CIVILITE) ?></td>
+                                <td><?php e($record->NOM_CONTACT) ?></td>
 				<td><?php e($record->PRENOM) ?></td>
-				<td><?php e($record->NOM_CONTACT) ?></td>
-				<td><?php e($record->MAIL) ?></td>
-				<td><?php e($record->ID_STR) ?></td>
+				<td>
+                                    <?php echo mailto($record->MAIL, '<i class="icon-envelope">&nbsp;</i> ' .  $record->MAIL, 'target="_blank"'); ?>
+                                </td>
+                                
+                                <?php if ($this->auth->has_permission('Contacts.Loireatlantique.Edit')) : ?>
+				<td><?php echo anchor(SITE_AREA .'/loireatlantique/contacts/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>Modifier') ?></td>
+				<?php else: ?>
+				
+				<?php endif; ?>
+                                
 				</tr>
 			<?php endforeach; ?>
 			<?php else: ?>
 				<tr>
-					<td colspan="6">No records found that match your selection.</td>
+					<td colspan="7"><?php echo lang('bf_no_record_found'); ?></td>
 				</tr>
 			<?php endif; ?>
 			</tbody>
