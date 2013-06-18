ede84509ce3eb31a6aadfb3018e1d5f4249f8ec2
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-18 12:08:26 +0200

    Modification du nom de la fonction list_scope() et des noms de variables.

--- application/modules/scope/models/scope_model.php
+++ application/modules/scope/models/scope_model.php
@@ -2,25 +2,26 @@
 
 class Scope_model extends BF_Model {
 
-	protected $table		= "pcet_ges_bilan";
-	protected $key			= "id";
+	protected $table	= "pcet_ges_bilan";
+	protected $key		= "id";
 	protected $soft_deletes	= false;
 	protected $date_format	= "datetime";
 	protected $set_created	= false;
 	protected $set_modified = false;
         
-        function get_scope() {
-		$query = $this->db->get('pcet_ges_bilan');
+        function list_scopes() {
+            $query = $this->db->get('pcet_ges_bilan');
 		 
-		$scope = array();
+            $scopes = array();
 		 
-		if ($query->result()) {
-			foreach ($query->result() as $scopes) {
-				$scope[$scopes->id] = $scopes->NOM_GES_BILAN;
-			}
-			return $scope;
-		} else {
-			return FALSE;
+            if ($query->result()) {
+		foreach ($query->result() as $scope) {
+                	$scopes[$scope->id] = $scope->NOM_GES_BILAN;
 		}
+		return $scopes;
+            } else {
+		return FALSE;
+            }
 	}
+
 }
