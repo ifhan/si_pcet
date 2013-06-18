7e5c36808b789a42842090189c72565fa01a259f
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-18 12:20:41 +0200

    Modification du descriptif du module.

--- application/modules/adaptation/config/config.php
+++ application/modules/adaptation/config/config.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');$config['module_config'] = array(
-	'description'	=> 'Suivi du Volet Adaptation PCET',
+	'description'	=> 'Suivi du Volet Adaptation des PCET',
 	'name'		=> '7. Volet Adaptation',
 	'version'		=> '0.0.2',
 	'author'		=> 'Anthony Tolone'
