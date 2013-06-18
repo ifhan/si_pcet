2e7910ee329b4536c9eefbce3bcfec25f7278ec0
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-18 15:07:51 +0200

    Mise en forme et modification des variables de langage.

--- application/modules/diagnostic/language/french/diagnostic_lang.php
+++ application/modules/diagnostic/language/french/diagnostic_lang.php
@@ -1,36 +1,36 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$lang['diagnostic']			= 'Volet "Diagnostic"';
+$lang['diagnostic']                             = 'Volet "Diagnostic"';
 $lang['diagnostic_title']			= 'Volet "Diagnostic" des PCET de ';
 $lang['diagnostic_manage']			= 'Suivre les diagnostics';
-$lang['diagnostic_edit']				= 'Modifier le suivi du Volet "Diagnostic"';
-$lang['diagnostic_true']				= 'Vrai';
-$lang['diagnostic_false']				= 'Faux';
+$lang['diagnostic_edit']			= 'Modifier le suivi du Volet "Diagnostic"';
+$lang['diagnostic_true']			= 'Vrai';
+$lang['diagnostic_false']			= 'Faux';
 $lang['diagnostic_create']			= 'Ajouter un suivi du Volet "Diagnostic" pour un PCET';
-$lang['diagnostic_list']				= 'Liste';
+$lang['diagnostic_list']			= 'Liste';
 $lang['diagnostic_new']				= 'Nouveau';
 $lang['diagnostic_edit_text']			= 'Edit this to suit your needs';
 $lang['diagnostic_no_records']			= 'There aren\'t any diagnostic in the system.';
 $lang['diagnostic_create_new']			= 'Create a new Diagnostic.';
-$lang['diagnostic_create_success']			= 'Diagnostic successfully created.';
-$lang['diagnostic_create_failure']			= 'There was a problem creating the diagnostic: ';
-$lang['diagnostic_create_new_button']			= 'Create New Diagnostic';
+$lang['diagnostic_create_success']		= 'Diagnostic successfully created.';
+$lang['diagnostic_create_failure']		= 'There was a problem creating the diagnostic: ';
+$lang['diagnostic_create_new_button']		= 'Create New Diagnostic';
 $lang['diagnostic_invalid_id']			= 'Invalid Diagnostic ID.';
-$lang['diagnostic_edit_success']			= 'Diagnostic successfully saved.';
-$lang['diagnostic_edit_failure']			= 'There was a problem saving the diagnostic: ';
-$lang['diagnostic_delete_success']			= 'record(s) successfully deleted.';
-$lang['diagnostic_delete_failure']			= 'We could not delete the record: ';
-$lang['diagnostic_delete_error']			= 'You have not selected any records to delete.';
+$lang['diagnostic_edit_success']		= 'Diagnostic successfully saved.';
+$lang['diagnostic_edit_failure']		= 'There was a problem saving the diagnostic: ';
+$lang['diagnostic_delete_success']		= 'enregistrement(s) supprimés avec succès.';
+$lang['diagnostic_delete_failure']		= 'We could not delete the record: ';
+$lang['diagnostic_delete_error']		= 'You have not selected any records to delete.';
 $lang['diagnostic_actions']			= 'Actions';
 $lang['diagnostic_cancel']			= 'Annuler';
-$lang['diagnostic_delete_record']			= 'Supprimer';
-$lang['diagnostic_delete_confirm']			= 'Are you sure you want to delete this diagnostic?';
-$lang['diagnostic_edit_heading']			= 'Edit Diagnostic';
+$lang['diagnostic_delete_record']		= 'Supprimer';
+$lang['diagnostic_delete_confirm']		= 'Are you sure you want to delete this diagnostic?';
+$lang['diagnostic_edit_heading']		= 'Edit Diagnostic';
 
 // Create/Edit Buttons
-$lang['diagnostic_action_edit']				= 'Enregistrer';
-$lang['diagnostic_action_create']				= 'Enregistrer';
+$lang['diagnostic_action_edit']			= 'Enregistrer';
+$lang['diagnostic_action_create']		= 'Enregistrer';
 
 // Activities
-$lang['diagnostic_act_create_record']			= 'Created record with ID';
-$lang['diagnostic_act_edit_record']			= 'Updated record with ID';
-$lang['diagnostic_act_delete_record']			= 'Deleted record with ID';
+$lang['diagnostic_act_create_record']		= 'Created record with ID';
+$lang['diagnostic_act_edit_record']		= 'Updated record with ID';
+$lang['diagnostic_act_delete_record']		= 'Deleted record with ID';
