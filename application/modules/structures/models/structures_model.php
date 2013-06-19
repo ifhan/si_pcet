bc99e6dc2d106bcf344e9c8afc6773cf1d4c4eb6
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-19 12:23:59 +0200

    Ajout d'une fonction get_structure_by_id_str($ID_STR)

--- application/modules/structures/models/structures_model.php
+++ application/modules/structures/models/structures_model.php
@@ -76,6 +76,25 @@ class Structures_model extends BF_Model {
                     ->find_by('ID_PCET',$ID_PCET);
             return $structure;
         }
+        
+        function get_structure_by_id_str($ID_STR) {
+            
+            $structure = $this->structures_model
+                    ->join('pcet_type_str','pcet_type_str.id = pcet_structure.TYPE_STRUCTURE_id','left')
+                    ->select('pcet_structure.ID_STR as ID_STR, pcet_structure.DEPARTEMENT_id as DEPARTEMENT_id, pcet_type_str.NOM_TYPE as NOM_TYPE')
+                    ->join('bdc_commune_52','bdc_commune_52.INSEE_Commune = pcet_structure.ID_STR','left')
+                    ->join('bdc_departement_52','bdc_departement_52.INSEE_Departement = pcet_structure.ID_STR','left')
+                    ->join('n_epci_zsup_r52','n_epci_zsup_r52.SIREN_EPCI = pcet_structure.ID_STR','left')
+                    ->join('r_pays_contour_r52','r_pays_contour_r52.id_pays = pcet_structure.ID_STR','left')
+                    ->join('r_pnr_r52','r_pnr_r52.id_regional = pcet_structure.ID_STR','left')
+                    ->select('bdc_commune_52.Nom_Commune as Nom_Commune')
+                    ->select('bdc_departement_52.Nom_Departement as Nom_Departement')
+                    ->select('n_epci_zsup_r52.NOM_EPCI as NOM_EPCI')
+                    ->select('r_pays_contour_r52.nom as nom_pays')
+                    ->select('r_pnr_r52.nom as nom_pnr')
+                    ->find_by('ID_STR',$ID_STR);
+            return $structure;
+        }
     
     
 	
