90f04f47143ba4c59db10ea8e637a6e4e21568fe
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-21 11:58:59 +0200

    Modification de la fonciton show()

--- application/modules/pcet/controllers/pcet.php
+++ application/modules/pcet/controllers/pcet.php
@@ -13,6 +13,9 @@ class pcet extends Front_Controller {
 		$this->load->model('pcet_model', null, true);
 		$this->load->model('structures/structures_model', null, true);
 		$this->load->model('phases/phases_model', null, true);
+                $this->load->model('avis/avis_model', null, true);
+                $this->load->model('diagnostic/diagnostic_model', null, true);
+                $this->load->model('indicateur/indicateur_model', null, true);
                 $this->load->model('adaptation/adaptation_model', null, true);
                 $this->load->helper('typography');
 		$this->lang->load('pcet');
@@ -48,14 +51,20 @@ class pcet extends Front_Controller {
 	public function show($ID_PCET)
 	{
 
-            $structure = $this->structures_model->get_structure_by_id($ID_PCET);
-            $phase = $this->phases_model->get_phase_by_id($ID_PCET);
+            $structure = $this->structures_model->get_structure_by_id_pcet($ID_PCET);
+            $phase = $this->phases_model->get_phase_by_id_pcet($ID_PCET);
             $pcet = $this->pcet_model->find_by('ID_PCET',$ID_PCET);
+            $avis = $this->avis_model->get_avis_by_id_pcet($ID_PCET);
+            $diagnostic = $this->diagnostic_model->get_diagnostic_by_id_pcet($ID_PCET);
+            $indicateur = $this->indicateur_model->get_indicateur_by_id_pcet($ID_PCET);
             $adaptation = $this->adaptation_model->find_by('ID_PCET',$ID_PCET);
 		
             Template::set('pcet', $pcet);
             Template::set('structure', $structure);
             Template::set('phase', $phase);
+            Template::set('avis', $avis);
+            Template::set('diagnostic', $diagnostic);
+            Template::set('indicateur', $indicateur);  
             Template::set('adaptation', $adaptation);
             Template::set('toolbar_title', lang('pcet_list'));
             Template::render();
