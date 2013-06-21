92f543a26add235ada85dac9e1dfde353fbf723f
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-21 16:27:10 +0200

    Typo.

--- application/modules/gouvernance/views/loireatlantique/create.php
+++ application/modules/gouvernance/views/loireatlantique/create.php
@@ -27,7 +27,7 @@ $id = isset($gouvernance['id']) ? $gouvernance['id'] : '';
 			<?php echo form_dropdown('gouvernance_ID_PCET',$pcets,set_value('gouvernance_ID_PCET', isset($pcets['ID_PCET']) ? $pcets['ID_PCET'] : ''),'Sélectionner un PCET');?>
 
 			<div class="control-group <?php echo form_error('PRESENCE_GOUV') ? 'error' : ''; ?>">
-				<?php echo form_label('Presence de gouvernance', 'gouvernance_PRESENCE_GOUV', array('class' => 'control-label') ); ?>
+				<?php echo form_label('Mise en place d\'une gouvernance ?', 'gouvernance_PRESENCE_GOUV', array('class' => 'control-label') ); ?>
 				<div class='controls'>
 					<label class='checkbox' for='gouvernance_PRESENCE_GOUV'>
 						<input type='checkbox' id='gouvernance_PRESENCE_GOUV' name='gouvernance_PRESENCE_GOUV' value='1' <?php echo (isset($gouvernance['PRESENCE_GOUV']) && $gouvernance['PRESENCE_GOUV'] == 1) ? 'checked="checked"' : set_checkbox('gouvernance_PRESENCE_GOUV', 1); ?>>
@@ -37,7 +37,7 @@ $id = isset($gouvernance['id']) ? $gouvernance['id'] : '';
 			</div>
 
 			<div class="control-group <?php echo form_error('ACTEURS_GOUV') ? 'error' : ''; ?>">
-				<?php echo form_label('Les acteurs associes', 'gouvernance_ACTEURS_GOUV', array('class' => 'control-label') ); ?>
+				<?php echo form_label('Acteurs associés', 'gouvernance_ACTEURS_GOUV', array('class' => 'control-label') ); ?>
 				<div class='controls'>
 					<?php echo form_textarea( array( 'name' => 'gouvernance_ACTEURS_GOUV', 'id' => 'gouvernance_ACTEURS_GOUV', 'rows' => '5', 'cols' => '80', 'value' => set_value('gouvernance_ACTEURS_GOUV', isset($gouvernance['ACTEURS_GOUV']) ? $gouvernance['ACTEURS_GOUV'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('ACTEURS_GOUV'); ?></span>
