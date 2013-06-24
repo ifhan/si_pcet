8be7f67e5feba8ec7383567039952fa477630f49
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-24 10:33:24 +0200

    Traduction des variables.

--- bonfire/modules/settings/language/french/settings_lang.php
+++ bonfire/modules/settings/language/french/settings_lang.php
@@ -1,23 +1,23 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang['set_allow_name_change_note']	= 'Allow users to change their display name after registering?';
-$lang['set_name_change_frequency']	= ' Changes every';
-$lang['set_days']					= ' days';
-$lang['settings_saved_success']     = 'Your settings were successfully saved.';
-$lang['settings_error_success']     = 'There was an error saving your settings.';
+$lang['set_allow_name_change_note']	= 'Permettre aux utilisateurs de changer l\'affichage de leur nom après l\'enregistrement ?';
+$lang['set_name_change_frequency']	= ' Changer tous les ';
+$lang['set_days']					= ' jours';
+$lang['settings_saved_success']     = 'Vos paramètres ont été sauvegardés avec succès.';
+$lang['settings_error_success']     = 'Il y a eu une erreur lors de la sauvegarde de vos paramètres.';
 
 
-$lang['set_tab_settings']			= 'Main Settings';
-$lang['set_tab_security']			= 'Security Settings';
-$lang['set_tab_developer']			= 'Developer Settings';
-$lang['set_tab_extended']			= 'Extended Settings';
+$lang['set_tab_settings']			= 'Paramètres principaux';
+$lang['set_tab_security']			= 'Paramètres de sécurité';
+$lang['set_tab_developer']			= 'Paramètres de développeur';
+$lang['set_tab_extended']			= 'Paramètres étendus';
 
-$lang['set_option_password']		= 'Password Options';
-$lang['set_option_developer']		= 'Developer Options';
-$lang['set_option_extended']		= 'Extended Settings';
+$lang['set_option_password']		= 'Options du mot de passe';
+$lang['set_option_developer']		= 'Options du développeur';
+$lang['set_option_extended']		= 'Paramètres étendus';
 
-$lang['set_password_iterations']	= 'Password Stretching';
-$lang['set_force_reset']			= 'Force Password Resets';
-$lang['set_reset_note']				= 'This forces all users to reset their passwords on their next login. This will log you out without saving other settings.';
-$lang['set_reset']					= 'Reset Now';
-$lang['set_password_reset_confirm']	= 'Really force password reset for all users? This will log you out immediately.';
+$lang['set_password_iterations']	= 'Stretching du mot de passe';
+$lang['set_force_reset']			= 'Forcer la réinitialisation des mots de passe';
+$lang['set_reset_note']				= 'Cette action force tous les utilisateurs à réinitialiser leur mot de passe à la prochaine connexion. Cela va vous déconnecter sans sauvegarder d\'autres paramètres.';
+$lang['set_reset']					= 'Réinitialiser maintenant';
+$lang['set_password_reset_confirm']	= 'Réellement forcer la réinitialisation du mot de passe pour tous les utilisateurs ? Cela va vous déconnecter immédiatement.';
