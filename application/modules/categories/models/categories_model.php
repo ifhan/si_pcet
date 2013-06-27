d2fd4155f393b7e332e22d3dee16708723ab91f5
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-27 16:18:05 +0200

    Ajout d'une fonction list_categories()

--- application/modules/categories/models/categories_model.php
+++ application/modules/categories/models/categories_model.php
@@ -2,10 +2,38 @@
 
 class Categories_model extends BF_Model {
 
-	protected $table		= "pcet_categories_aide";
-	protected $key			= "id";
+	protected $table	= "pcet_categories_aide";
+	protected $key		= "id";
 	protected $soft_deletes	= false;
 	protected $date_format	= "datetime";
 	protected $set_created	= false;
 	protected $set_modified = false;
+
+        /*
+		Method: list_categories()
+
+		Sélectionne l'ensemble des catégories de l'aide 
+        *       pour affichage dans une liste.
+	*/           
+        public function list_categories() {
+            		
+            $query = $this->db->get('pcet_categories_aide');
+		 
+            $categories = array();
+		 	
+            if ($query->result()) {
+                
+		foreach ($query->result() as $categorie) {
+                    
+                    $categories[$categorie->id] = $categorie->NOM_CATEGORIE;
+		}
+                
+		return $categories;
+
+           } else {
+               
+		return FALSE;
+           }
+
+    }
 }
