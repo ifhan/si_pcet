76ca394154ccdc69aa7e35f302aff483ac5aab37
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-06-27 17:19:42 +0200

    Ajout d'une variable dans le select() de show().

--- application/modules/aide/models/aide_model.php
+++ application/modules/aide/models/aide_model.php
@@ -14,7 +14,8 @@ class Aide_model extends BF_Model {
         public function get_aides() {
             $records = $this->aide_model
                     ->join('pcet_categories_aide','pcet_aide.category_id = pcet_categories_aide.id','left')
-                    ->select('pcet_aide.id as id, pcet_aide.title as title, pcet_categories_aide.NOM_CATEGORIE as NOM_CATEGORIE')
+                    ->select('pcet_aide.id as id, pcet_aide.number as number, pcet_aide.title as title, 
+                        pcet_categories_aide.NOM_CATEGORIE as NOM_CATEGORIE, pcet_categories_aide.number as categories_number')
                     ->order_by('title','asc')
                     ->find_all();
             return $records;
