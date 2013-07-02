e557e06b646d7c1e65ce275e98aed4198a9adcd5
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-02 17:35:30 +0200

    Mise à jour du menu.

--- application/modules/aide/views/content/_sub_nav.php
+++ application/modules/aide/views/content/_sub_nav.php
@@ -2,11 +2,11 @@
     <li <?php echo $this->uri->segment(3) == '' ? 'class="active"' : '' ?>>
 	<a href="<?php echo site_url(SITE_AREA .'/content') ?>" id="home"><?php echo lang('bf_home'); ?></a>
     </li>
-    <li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-        <a href="<?php echo site_url(SITE_AREA .'/content/aide') ?>"><?php echo lang('aide_summary'); ?></a>
+    <li <?php echo $this->uri->segment(4) == 'summary' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/aide/summary') ?>"><?php echo lang('aide_summary'); ?></a>
     </li>    
-    <li <?php echo $this->uri->segment(4) == 'list' ? 'class="active"' : '' ?>>
-        <a href="<?php echo site_url(SITE_AREA .'/content/aide/list') ?>"><?php echo lang('aide_list'); ?></a>
+    <li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
+        <a href="<?php echo site_url(SITE_AREA .'/content/aide') ?>"><?php echo lang('aide_list'); ?></a>
     </li>
     <li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?>>
         <a href="<?php echo site_url(SITE_AREA .'/content/aide/create') ?>"><?php echo lang('aide_new'); ?></a>
