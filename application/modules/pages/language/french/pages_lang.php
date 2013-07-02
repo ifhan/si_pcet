a6a440109f8bc598feb58e951c447b3353fb6f97
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-02 13:54:40 +0200

    Modification des variables de langage.

---
+++ application/modules/pages/language/french/pages_lang.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['pages']                          = 'Pages';
+$lang['pages_manage']			= 'Paramétrer les pages';
+$lang['pages_edit']			= 'Modifier une page';
+$lang['pages_true']			= 'Oui';
+$lang['pages_false']			= 'Non';
+$lang['pages_create']			= 'Créer une page';
+$lang['pages_list']			= 'Liste';
+$lang['pages_new']			= 'Nouvelle';
+$lang['pages_edit_text']		= 'Edit this to suit your needs';
+$lang['pages_no_records']		= 'There aren\'t any pages in the system.';
+$lang['pages_create_new']		= 'Create a new Pages.';
+$lang['pages_create_success']		= 'Page créée avec succès.';
+$lang['pages_create_failure']		= 'There was a problem creating the pages: ';
+$lang['pages_create_new_button']	= 'Create New Pages';
+$lang['pages_invalid_id']		= 'Invalid Pages ID.';
+$lang['pages_edit_success']		= 'Pages successfully saved.';
+$lang['pages_edit_failure']		= 'There was a problem saving the pages: ';
+$lang['pages_delete_success']		= 'enregistrement(s) supprimés avec succès.';
+$lang['pages_delete_failure']		= 'We could not delete the record: ';
+$lang['pages_delete_error']		= 'You have not selected any records to delete.';
+$lang['pages_actions']			= 'Actions';
+$lang['pages_cancel']			= 'Annuler';
+$lang['pages_delete_record']		= 'Supprimer';
+$lang['pages_delete_confirm']		= 'Are you sure you want to delete this pages?';
+$lang['pages_edit_heading']		= 'Edit Pages';
+
+// Create/Edit Buttons
+$lang['pages_action_edit']		= 'Enregistrer';
+$lang['pages_action_create']		= 'Enregistrer';
+
+// Activities
+$lang['pages_act_create_record']	= 'Created record with ID';
+$lang['pages_act_edit_record']		= 'Updated record with ID';
+$lang['pages_act_delete_record']	= 'Deleted record with ID';
