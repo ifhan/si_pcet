a3c765007d8a14ed9f29c45fe0d4d87bfaea1c38
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-03 11:06:39 +0200

    Ajout d'un lien vers l'accueil.

--- application/modules/types/views/content/_sub_nav.php
+++ application/modules/types/views/content/_sub_nav.php
@@ -1,4 +1,7 @@
 <ul class="nav nav-pills">
+	<li>
+            <a href="<?php echo site_url(SITE_AREA .'/content') ?>" id="home"><?php echo lang('bf_home'); ?></a>
+	</li>      
 	<li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
 		<a href="<?php echo site_url(SITE_AREA .'/content/types') ?>" id="list"><?php echo lang('types_list'); ?></a>
 	</li>
