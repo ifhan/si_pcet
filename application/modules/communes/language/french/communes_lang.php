a997d593a94321482fe11f25b4e6a7b739c0fa56
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-03 12:02:29 +0200

    First commit.

---
+++ application/modules/communes/language/french/communes_lang.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['communes_manage']			= 'Gérer les communes';
+$lang['communes_edit']				= 'Edit';
+$lang['communes_true']				= 'Vrai';
+$lang['communes_false']				= 'Faux';
+$lang['communes_create']			= 'Create';
+$lang['communes_list']				= 'Liste';
+$lang['communes_new']				= 'Nouvelle';
+$lang['communes_edit_text']			= 'Edit this to suit your needs';
+$lang['communes_no_records']			= 'There aren\'t any communes in the system.';
+$lang['communes_create_new']			= 'Create a new Communes.';
+$lang['communes_create_success']		= 'Communes successfully created.';
+$lang['communes_create_failure']		= 'There was a problem creating the communes: ';
+$lang['communes_create_new_button']		= 'Create New Communes';
+$lang['communes_invalid_id']			= 'Invalid Communes ID.';
+$lang['communes_edit_success']			= 'Communes successfully saved.';
+$lang['communes_edit_failure']			= 'There was a problem saving the communes: ';
+$lang['communes_delete_success']		= 'enregistrement(s) supprimés avec succès.';
+$lang['communes_delete_failure']		= 'We could not delete the record: ';
+$lang['communes_delete_error']			= 'You have not selected any records to delete.';
+$lang['communes_actions']			= 'Actions';
+$lang['communes_cancel']			= 'Annuler';
+$lang['communes_delete_record']			= 'Supprimer';
+$lang['communes_delete_confirm']		= 'Are you sure you want to delete this communes?';
+$lang['communes_edit_heading']			= 'Edit Communes';
+
+// Create/Edit Buttons
+$lang['communes_action_edit']			= 'Enregistrer';
+$lang['communes_action_create']			= 'Enregistrer';
+
+// Activities
+$lang['communes_act_create_record']		= 'Created record with ID';
+$lang['communes_act_edit_record']		= 'Updated record with ID';
+$lang['communes_act_delete_record']		= 'Deleted record with ID';
---
+++ application/modules/departements/language/french/departements_lang.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['departements']                           = 'Départements';
+$lang['departements_manage']			= 'Gérer les départements';
+$lang['departements_edit']			= 'Modifier un département';
+$lang['departements_true']			= 'Vrai';
+$lang['departements_false']			= 'Faux';
+$lang['departements_create']			= 'Ajouter un département';
+$lang['departements_list']			= 'Liste';
+$lang['departements_title']			= 'Liste des départements en Pays de la Loire';
+$lang['departements_new']			= 'Nouveau';
+$lang['departements_edit_text']			= 'Edit this to suit your needs';
+$lang['departements_no_records']		= 'There aren\'t any departements in the system.';
+$lang['departements_create_new']		= 'Create a new Departements.';
+$lang['departements_create_success']		= 'Departements successfully created.';
+$lang['departements_create_failure']		= 'There was a problem creating the departements: ';
+$lang['departements_create_new_button']		= 'Create New Departements';
+$lang['departements_invalid_id']		= 'Invalid Departements ID.';
+$lang['departements_edit_success']		= 'Departements successfully saved.';
+$lang['departements_edit_failure']		= 'There was a problem saving the departements: ';
+$lang['departements_delete_success']		= 'enregistrement(s) supprimés avec succès.';
+$lang['departements_delete_failure']		= 'We could not delete the record: ';
+$lang['departements_delete_error']		= 'You have not selected any records to delete.';
+$lang['departements_actions']			= 'Actions';
+$lang['departements_cancel']			= 'Annuler';
+$lang['departements_delete_record']		= 'Supprimer';
+$lang['departements_delete_confirm']		= 'Are you sure you want to delete this departements?';
+$lang['departements_edit_heading']		= 'Edit Departements';
+
+// Create/Edit Buttons
+$lang['departements_action_edit']		= 'Enregistrer';
+$lang['departements_action_create']		= 'Enregistrer';
+
+// Activities
+$lang['departements_act_create_record']		= 'Created record with ID';
+$lang['departements_act_edit_record']		= 'Updated record with ID';
+$lang['departements_act_delete_record']		= 'Deleted record with ID';
