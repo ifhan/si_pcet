819ed0b358b0367647064584404c62719ab445ce
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-04 15:59:10 +0200

    First commit.

---
+++ application/modules/autres/language/french/autres_lang.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['autres']                         = 'Autres collectivités';
+$lang['autres_manage']			= 'Gérer les autres collectivités';
+$lang['autres_edit']			= 'Modifier une collectivité';
+$lang['autres_true']			= 'Vrai';
+$lang['autres_false']			= 'Faux';
+$lang['autres_create']			= 'Ajouter une collectivité';
+$lang['autres_list']			= 'Liste';
+$lang['autres_new']			= 'Nouvelle';
+$lang['autres_edit_text']		= 'Edit this to suit your needs';
+$lang['autres_no_records']		= 'There aren\'t any autres in the system.';
+$lang['autres_create_new']		= 'Create a new Autres.';
+$lang['autres_create_success']		= 'Autres successfully created.';
+$lang['autres_create_failure']		= 'There was a problem creating the autres: ';
+$lang['autres_create_new_button']	= 'Create New Autres';
+$lang['autres_invalid_id']		= 'Invalid Autres ID.';
+$lang['autres_edit_success']		= 'Autres successfully saved.';
+$lang['autres_edit_failure']		= 'There was a problem saving the autres: ';
+$lang['autres_delete_success']		= 'enregistrement(s) supprimés avec succès.';
+$lang['autres_delete_failure']		= 'We could not delete the record: ';
+$lang['autres_delete_error']		= 'You have not selected any records to delete.';
+$lang['autres_actions']			= 'Actions';
+$lang['autres_cancel']			= 'Annuler';
+$lang['autres_delete_record']		= 'Supprimer';
+$lang['autres_delete_confirm']		= 'Are you sure you want to delete this autres?';
+$lang['autres_edit_heading']		= 'Edit Autres';
+
+// Create/Edit Buttons
+$lang['autres_action_edit']		= 'Enregistrer';
+$lang['autres_action_create']		= 'Enregistrer';
+
+// Activities
+$lang['autres_act_create_record']	= 'Created record with ID';
+$lang['autres_act_edit_record']		= 'Updated record with ID';
+$lang['autres_act_delete_record']	= 'Deleted record with ID';
