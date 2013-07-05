be9884d84814c113c701e89c670b6ac46048c484
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-07-05 15:06:01 +0200

    Ajout d'une fonction get_departements().

--- application/modules/departements/models/departements_model.php
+++ application/modules/departements/models/departements_model.php
@@ -10,6 +10,29 @@ class Departements_model extends BF_Model {
 	protected $set_modified = false;
 
         /*
+		Method: get_departements()
+
+		Sélectionne tous les départements de la région.
+	*/           
+	function get_departements() {
+		$this->db->select('INSEE_Departement, Nom_Departement')
+			->order_by('INSEE_Departement', 'asc');
+		$query = $this->db->get('bdc_departement_52');
+		 
+		$departements = array();
+		 
+		if ($query->result()) {
+			foreach ($query->result() as $departement) {
+				$departements[$departement->INSEE_Departement] = $departement->Nom_Departement;
+			}
+			return $departements;
+		} else {
+			return FALSE;
+		}
+	}        
+        
+        
+        /*
 		Method: get_departement_by_insee_departement()
 
 		Sélectionne un département par son code INSEE.
