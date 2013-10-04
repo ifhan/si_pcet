986a8bd0b55ee7cd3ee90b9ff801aef508683cbd
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-04 13:57:43 +0200

    Création de la vue show.php

---
+++ application/modules/avis/views/admin/show.php
@@ -0,0 +1,73 @@
+<div>
+    <h3><?php e($avis->ID_PCET.' - '.$structure->NOM_TYPE.' - '.$structure->Nom_Commune.$structure->Nom_Departement.$structure->NOM_EPCI.$structure->nom_pays.$structure->nom_pnr) ?>
+    </h3>
+</div><br />
+<table class="table table-striped table-bordered">
+    <tbody>
+        <tr>
+            <th>Date de la sollicitation de l'avis de l'État</th>
+            <td>
+                <?php if($avis->DEM_ETAT_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->DEM_ETAT_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Date du rendu de l'avis de l'État</th>
+            <td>
+                <?php if($avis->REP_ETAT_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->REP_ETAT_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Date de la sollicitation de l'avis du Conseil régional</th>
+            <td>
+                <?php if($avis->DEM_REG_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->DEM_REG_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Date du rendu de l'avis du Conseil régional</th>
+            <td>
+                <?php if($avis->REP_REG_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->REP_REG_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Date de la sollicitation de l'avis de l'ADEME</th>
+            <td>
+                <?php if($avis->DEM_ADEME_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->DEM_ADEME_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Date du rendu de l'avis de l'ADEME</th>
+            <td>
+                <?php if($avis->REP_ADEME_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->REP_ADEME_AVIS)))) ?>
+                <?php endif; ?>
+            </td>
+        </tr>
+        <tr>
+            <th>Points positifs</th>
+            <td><?php echo auto_typography($avis->PP_AVIS) ?></td>
+        </tr>
+        <tr>
+            <th>Points négatifs</th>
+            <td><?php echo auto_typography($avis->PP_AVIS) ?></td>
+        </tr> 
+        <tr>
+            <th>Date d'adoption du PCET</th>
+            <td>
+                <?php if($avis->DATE_ADOPT_AVIS !== '0000-00-00'): ?>
+                    <?php e(date("d/m/Y", strtotime(($avis->DATE_ADOPT_AVIS)))) ?>
+                 <?php endif; ?>
+            </td>
+        
+        </tr>
+    </tbody>
+</table>
