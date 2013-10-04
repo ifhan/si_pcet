a161aedbfb3c86ad9ad4b357ab8779a7af34961f
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-04 13:56:33 +0200

    Modification du tableau

--- application/modules/engagement/views/vendee/index.php
+++ application/modules/engagement/views/vendee/index.php
@@ -4,18 +4,15 @@
 		<table class="table table-striped">
 			<thead>
 				<tr>
-					<?php if ($this->auth->has_permission('Engagement.Vendee.Delete') && isset($records) && is_array($records) && count($records)) : ?>
+                                    <?php if ($this->auth->has_permission('Engagement.Vendee.Delete') && isset($records) && is_array($records) && count($records)) : ?>
 					<th class="column-check"><input class="check-all" type="checkbox" /></th>
-					<?php endif;?>
-					
-					<th>PCET</th>
-					<th>Date de déliberation</th>
-					<th>Date du courrier de la collectivité notifiant sa déliberation a l'État</th>
-					<th>Date du courrier de la collectivité notifiant sa déliberation au Conseil regional</th>
-					<th>Date courrier de notification d'engagement de la collectivité à l'USH</th>
-					<th>Consultation aval de l'USH souhaitée ?</th>
-					<th>Date du courrier de réponse de l'USH</th>
-					<th>Date du Porter-à-connaissance de l'État</th>
+                                    <?php endif;?>
+                                        
+                                    <th>PCET</th>
+                                    <th>Date de déliberation</th>
+                                    <th>Consultation aval de l'USH souhaitée ?</th>
+                                    <th>Date du Porter-à-connaissance de l'État</th>
+                                    <th>Actions</th>
 				</tr>
 			</thead>
 			<?php if (isset($records) && is_array($records) && count($records)) : ?>
@@ -34,51 +31,35 @@
 			<?php if (isset($records) && is_array($records) && count($records)) : ?>
 			<?php foreach ($records as $record) : ?>
 				<tr>
-					<?php if ($this->auth->has_permission('Engagement.Vendee.Delete')) : ?>
-					<td><input type="checkbox" name="checked[]" value="<?php echo $record->id ?>" /></td>
-					<?php endif;?>
-					
-				<?php if ($this->auth->has_permission('Engagement.Vendee.Edit')) : ?>
-				<td><?php echo anchor(SITE_AREA .'/vendee/engagement/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>' .  $record->ID_PCET.' - '.$record->NOM_TYPE.' - '.$record->Nom_Commune.$record->Nom_Departement.$record->NOM_EPCI.$record->nom_pays.$record->nom_pnr) ?></td>
-				<?php else: ?>
-				<td><?php e($record->ID_PCET.' - '.$record->NOM_TYPE.' - '.$record->Nom_Commune.$record->Nom_Departement.$record->NOM_EPCI.$record->nom_pays.$record->nom_pnr) ?></td>
-				<?php endif; ?>
+                                    <?php if ($this->auth->has_permission('Engagement.Vendee.Delete')) : ?>
+                                    <td><input type="checkbox" name="checked[]" value="<?php echo $record->id ?>" /></td>
+                                    <?php endif;?>
+
+                                    <td><?php e($record->ID_PCET.' - '.$record->NOM_TYPE.' - '.$record->Nom_Commune.$record->Nom_Departement.$record->NOM_EPCI.$record->nom_pays.$record->nom_pnr) ?></td>
                                 
-				<td>
-                                    <?php if($record->DATE_DELIB !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->DATE_DELIB)))) ?>
-                                    <?php endif; ?>
-				<td>
-                                    <?php if($record->NOTIF_DELIB_ETAT !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_DELIB_ETAT)))) ?>
-                                    <?php endif; ?>
-                                </td>
-				<td>
-                                    <?php if($record->NOTIF_DELIB_CR !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_DELIB_CR)))) ?>
-                                    <?php endif; ?>
-                                </td>
-				<td>
-                                    <?php if($record->NOTIF_USH !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->NOTIF_USH)))) ?>
-                                    <?php endif; ?>
-                                </td>
-				<td>
-                                    <?php if($record->REP_USH == '1'): ?>
-                                        <?php echo "Oui"; ?>
-                                    <?php else: ?>
-                                        <?php echo "Non"; ?>
-                                    <?php endif; ?>
-				<td>
-                                    <?php if($record->DATE_REP_USH !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->DATE_REP_USH)))) ?>
-                                    <?php endif; ?>
-                                </td>
-				<td>
-                                    <?php if($record->DATE_PAC !== '0000-00-00'): ?>
-                                        <?php e(date("d/m/Y", strtotime(($record->DATE_PAC)))) ?>
-                                    <?php endif; ?>
-                                </td>
+                                    <td>
+                                        <?php if($record->DATE_DELIB !== '0000-00-00'): ?>
+                                            <?php e(date("d/m/Y", strtotime(($record->DATE_DELIB)))) ?>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td>
+                                        <?php if($record->REP_USH == '1'): ?>
+                                            <?php echo "Oui"; ?>
+                                        <?php else: ?>
+                                            <?php echo "Non"; ?>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td>
+                                        <?php if($record->DATE_PAC !== '0000-00-00'): ?>
+                                            <?php e(date("d/m/Y", strtotime(($record->DATE_PAC)))) ?>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td width="20%">
+                                        <?php echo anchor(SITE_AREA .'/vendee/engagement/show/'. $record->id, '<i class="icon-info-sign">&nbsp;</i>Voir l\'engagement de la démarche') ?><br />
+                                        <?php if ($this->auth->has_permission('Engagement.Vendee.Edit')) : ?>
+                                            <?php echo anchor(SITE_AREA .'/vendee/engagement/edit/'. $record->id, '<i class="icon-pencil">&nbsp;</i>Modifier') ?><br />
+                                        <?php endif; ?>
+                                    </td>
 				</tr>
 			<?php endforeach; ?>
 			<?php else: ?>
