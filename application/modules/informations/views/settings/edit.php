98751e9ad4b4184dc50924a1e7da70c18898a10a
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-07 16:09:06 +0200

    Adaptation du formulaire

--- application/modules/informations/views/settings/edit.php
+++ application/modules/informations/views/settings/edit.php
@@ -20,14 +20,14 @@ $id = isset($informations['id']) ? $informations['id'] : '';
 
 ?>
 <div class="admin-box">
-	<h3>Informations</h3>
+	<h3><?php echo lang('informations_edit')?></h3>
 	<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
 		<fieldset>
 
 			<div class="control-group <?php echo form_error('informations_title') ? 'error' : ''; ?>">
 				<?php echo form_label('Titre', 'informations_title', array('class' => 'control-label') ); ?>
 				<div class='controls'>
-					<?php echo form_textarea( array( 'name' => 'informations_title', 'id' => 'informations_title', 'rows' => '5', 'cols' => '80', 'value' => set_value('informations_title', isset($informations['informations_title']) ? $informations['informations_title'] : '') ) ); ?>
+					<?php echo form_textarea( array( 'name' => 'informations_title', 'id' => 'informations_title', 'class' => 'input-xxlarge', 'rows' => '1', 'cols' => '255', 'value' => set_value('informations_title', isset($informations['informations_title']) ? $informations['informations_title'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('informations_title'); ?></span>
 				</div>
 			</div>
@@ -35,25 +35,28 @@ $id = isset($informations['id']) ? $informations['id'] : '';
 			<div class="control-group <?php echo form_error('informations_slug') ? 'error' : ''; ?>">
 				<?php echo form_label('Slug', 'informations_slug', array('class' => 'control-label') ); ?>
 				<div class='controls'>
-					<?php echo form_textarea( array( 'name' => 'informations_slug', 'id' => 'informations_slug', 'rows' => '5', 'cols' => '80', 'value' => set_value('informations_slug', isset($informations['informations_slug']) ? $informations['informations_slug'] : '') ) ); ?>
+                                    <div class="input-prepend">
+                                      <span class="add-on"><?php echo site_url() .'informations/' ?></span>
+					<?php echo form_textarea( array( 'name' => 'informations_slug', 'id' => 'informations_slug', 'class' => 'input-xxlarge', 'rows' => '1', 'cols' => '80', 'value' => set_value('informations_slug', isset($informations['informations_slug']) ? $informations['informations_slug'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('informations_slug'); ?></span>
+                                    </div>
 				</div>
 			</div>
 
 			<div class="control-group <?php echo form_error('informations_text') ? 'error' : ''; ?>">
 				<?php echo form_label('Text', 'informations_text', array('class' => 'control-label') ); ?>
 				<div class='controls'>
-					<?php echo form_textarea( array( 'name' => 'informations_text', 'id' => 'informations_text', 'rows' => '5', 'cols' => '80', 'value' => set_value('informations_text', isset($informations['informations_text']) ? $informations['informations_text'] : '') ) ); ?>
+					<?php echo form_textarea( array( 'name' => 'informations_text', 'id' => 'informations_text', 'class' => 'input-xxlarge', 'rows' => '20', 'cols' => '200', 'value' => set_value('informations_text', isset($informations['informations_text']) ? $informations['informations_text'] : '') ) ); ?>
 					<span class='help-inline'><?php echo form_error('informations_text'); ?></span>
 				</div>
 			</div>
 
 			<div class="form-actions">
 				<input type="submit" name="save" class="btn btn-primary" value="<?php echo lang('informations_action_edit'); ?>"  />
-				or <?php echo anchor(SITE_AREA .'/settings/informations', lang('informations_cancel'), 'class="btn btn-warning"'); ?>
+				ou <?php echo anchor(SITE_AREA .'/settings/informations', lang('informations_cancel'), 'class="btn btn-warning"'); ?>
 				
 			<?php if ($this->auth->has_permission('Informations.Settings.Delete')) : ?>
-				or
+				ou
 				<button type="submit" name="delete" class="btn btn-danger" id="delete-me" onclick="return confirm('<?php e(js_escape(lang('informations_delete_confirm'))); ?>'); ">
 					<span class="icon-trash icon-white"></span>&nbsp;<?php echo lang('informations_delete_record'); ?>
 				</button>
