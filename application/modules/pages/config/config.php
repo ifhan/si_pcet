ac917b641ffc0ecab7547d93d962a155a74f9aba
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-07 16:03:43 +0200

    Module supprimé.

--- application/modules/pages/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');$config['module_config'] = array(
-	'description'	=> 'Informations du site',
-	'name'		=> 'Pages',
-	'version'		=> '0.0.1',
-	'author'		=> 'Ronan Vignard'
-);
--- application/modules/pages/views/show.php
+++
@@ -1,2 +0,0 @@
-<h4><?php e($page->title) ?></h4>
-<p><?php echo(auto_typography($page->text)) ?></p>
