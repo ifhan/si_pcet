10967eb49c3f2f70b04cc5f0b590cf7681062791
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-08 12:15:22 +0200

    Modification de la vue

--- application/modules/aide/views/summary.php
+++ application/modules/aide/views/summary.php
@@ -1,7 +1,20 @@
-<?php foreach ($categories as $categorie) : ?>
-    <h3><?php e($categorie->number.'. '.$categorie->NOM_CATEGORIE) ?></h3>
-    <?php foreach ($aides as $aide) : ?>
-        <h4><?php e($aide->number.'. '.$aide->title) ?></h4>
+<h3>1. Introduction</h3>
+    <?php foreach ($aides_1 as $aide) : ?>
+        <h4><?php echo anchor(SITE_AREA .'/content/aide/show/'. $aide->aide_number, $aide->aide_number.'. '.$aide->title) ?></h4>
     <?php endforeach; ?>
-<?php endforeach; ?>
-
+<h3>2. Acteurs</h3>        
+    <?php foreach ($aides_2 as $aide) : ?>
+        <h4><?php echo anchor(SITE_AREA .'/content/aide/show/'. $aide->aide_number, $aide->aide_number.'. '.$aide->title) ?></h4>
+    <?php endforeach; ?>
+<h3>3. PCET</h3>        
+    <?php foreach ($aides_3 as $aide) : ?>
+        <h4><?php echo anchor(SITE_AREA .'/content/aide/show/'. $aide->aide_number, $aide->aide_number.'. '.$aide->title) ?></h4>
+    <?php endforeach; ?>
+<h3>4. Avancement de la démarche</h3>        
+    <?php foreach ($aides_4 as $aide) : ?>
+        <h4><?php echo anchor(SITE_AREA .'/content/aide/show/'. $aide->aide_number, $aide->aide_number.'. '.$aide->title) ?></h4>
+    <?php endforeach; ?>
+<h3>5. Études & Analyses</h3>        
+    <?php foreach ($aides_5 as $aide) : ?>
+        <h4><?php echo anchor(SITE_AREA .'/content/aide/show/'. $aide->aide_number, $aide->aide_number.'. '.$aide->title) ?></h4>
+    <?php endforeach; ?>
