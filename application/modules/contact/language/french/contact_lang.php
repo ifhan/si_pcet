4415717d1510423cdb005b10133656e2f3b31ec3
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-08 10:00:48 +0200

    Modification de variable de langage.

--- application/modules/contact/language/french/contact_lang.php
+++ application/modules/contact/language/french/contact_lang.php
@@ -1,20 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $lang['contact_manage']				= 'Gérer les messages';
+$lang['contact_view']				= 'Voir un message';
 $lang['contact_edit_text']			= 'Edit this to suit your needs';
 $lang['contact_no_records']			= 'Il n\'y a pas de message dans le système.';
 $lang['contact_create_new']			= 'Créer un nouveau message.';
-$lang['contact_create_success']                 = 'Contact successfully created.';
+$lang['contact_create_success']                 = 'Message envoyé avec succès.';
 $lang['contact_create_failure']                 = 'Il y a eu un problème empêchant l\'envoi du message : ';
 $lang['contact_create_new_button']              = 'Create New Contact';
 $lang['contact_invalid_id']			= 'Invalid Contact ID.';
 $lang['contact_edit_success']                   = 'Contact successfully saved.';
-$lang['contact_edit_failure']                   = 'There was a problem saving the contact: ';
-$lang['contact_delete_success']                 = 'The Contact was successfully deleted.';
-$lang['contact_delete_failure']                 = 'We could not delete the contact: ';
+$lang['contact_edit_failure']                   = 'Il y a eu un problème à l\'enregistrement du message : ';
+$lang['contact_delete_success']                 = 'Message(s) supprimé(s) avec succès.';
+$lang['contact_delete_failure']                 = 'Le message n\'a pas pu être supprimé : ';
 $lang['contact_actions']			= 'Actions';
 $lang['contact_cancel']				= 'Annuler';
 $lang['contact_submit']				= 'Envoyer';
-$lang['contact_delete_record']                  = 'Delete this Contact';
+$lang['contact_delete_record']                  = 'Supprimer ce message';
 $lang['contact_delete_confirm']                 = 'Etes vous certain de vouloir supprimer ce message ?';
 $lang['contact_edit_heading']                   = 'Modifier le message';
 
@@ -24,5 +25,6 @@ $lang['contact_form_phone']			= 'Numéro de téléphone';
 $lang['contact_form_subject']                   = 'Sujet';
 $lang['contact_form_message']                   = 'Message';
 $lang['contact_form_created']                   = 'Créé le';
+$lang['contact_form_send']                      = 'Envoyé le';
 
 $lang['contact_form_success']                   = 'Merci d\'avoir pris le temps d\'envoyer votre message. Nous revenons vers vous dès que possible.';
