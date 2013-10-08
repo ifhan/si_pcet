79f90cd8c9a1b9858cf6e4461e8991da0a60fc17
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-08 09:57:40 +0200

    Ajout du lien vers l'accueil et traduction

--- application/modules/contact/views/content/_sub_nav.php
+++ application/modules/contact/views/content/_sub_nav.php
@@ -1,5 +1,8 @@
 <ul class="nav nav-pills">
+	<li>
+            <a href="<?php echo site_url(SITE_AREA .'/content') ?>" id="home"><?php echo lang('bf_home'); ?></a>
+	</li>     
 	<li <?php echo $this->uri->segment(4) == '' ? 'class="active"' : '' ?>>
-		<a href="<?php echo site_url(SITE_AREA .'/content/contact') ?>" id="list">All Messages</a>
+		<a href="<?php echo site_url(SITE_AREA .'/content/contact') ?>" id="list">Tous les messages</a>
 	</li>
 </ul>
