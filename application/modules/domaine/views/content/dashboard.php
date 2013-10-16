b40df182f86854d67e0031540ee15e481c3b0af3
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-10-16 16:28:11 +0200

    Modification du tableau

--- application/modules/domaine/views/content/dashboard.php
+++ application/modules/domaine/views/content/dashboard.php
@@ -5,18 +5,14 @@
 <table class="table table-striped">
         <thead>
             <tr>
-                <th>PCET</th>
                 <th>Nom de l'action</th>
-                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             <?php if (isset($records) && is_array($records) && count($records)) : ?>
             <?php foreach ($records as $record) : ?>
             <tr>
-                <td></td>
                 <td><?php echo auto_typography($record->NOM_ACTION); ?></td>
-                <td></td>
             </tr>
             <?php endforeach; ?>
             <?php else: ?>
