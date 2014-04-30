f6983e626ee4016a0130c5cde25ac66f25862030
Ronan Vignard <ronan.vignard@yahoo.fr>
2014-04-30 16:32:07 +0200

    Affichage conditionnel des valeurs du diagnostic

--- application/modules/pcet/views/admin/show.php
+++ application/modules/pcet/views/admin/show.php
@@ -239,21 +239,37 @@
                         <td><?php e($diagnostic->GES_DIAG) ?></td>
                     </tr>
                     <tr>
-                        <td><strong>Consommation du territoire (<abbr title="kilotonne d'équivalent pétrole">ktep</abbr>)</strong></td>
-                        <td><?php e($diagnostic->CONSO_KTEP_T) ?></td>
+                        <th>Consommation du territoire (<abbr title="kilotonne d'équivalent pétrole">ktep</abbr>)</th>
+                        <td>
+                            <?php if(!empty($diagnostic->CONSO_KTEP_T)): ?>
+                            <?php e($diagnostic->CONSO_KTEP_T) ?>
+                            <?php endif; ?>
+                        </td>
                     </tr>
                     <tr>
-                        <td><strong>Émissions du territoire (<abbr title="tonne équivalent CO2">teq-CO2</abbr>)</strong></td>
-                        <td><?php e($diagnostic->EMIS_CO2_T) ?></td>
+                        <th>Émissions du territoire (<abbr title="tonne équivalent CO2">teq-CO2</abbr>)</th>
+                        <td>
+                            <?php if(!empty($diagnostic->EMIS_CO2_T)): ?>
+                            <?php e($diagnostic->EMIS_CO2_T) ?>
+                            <?php endif; ?>
+                        </td>
                     </tr>
                     <tr>
-                        <td><strong>Consommation "Patrimoine et compétence" (<abbr title="kilotonne d'équivalent pétrole">ktep</abbr>)</strong></td>
-                        <td><?php e($diagnostic->CONSO_KTEP_PC) ?></td>
+                        <th>Consommation "Patrimoine et compétence" (<abbr title="kilotonne d'équivalent pétrole">ktep</abbr>)</th>
+                        <td>
+                            <?php if(!empty($diagnostic->CONSO_KTEP_PC)): ?>
+                            <?php e($diagnostic->CONSO_KTEP_PC) ?>
+                            <?php endif; ?>
+                        </td>
                     </tr>
                     <tr>
-                        <td><strong>Émissions "Patrimoine et compétence" (<abbr title="tonne équivalent CO2">teq-CO2</abbr>)</strong></td>
-                        <td><?php e($diagnostic->EMIS_CO2_PC) ?></td>
-                    </tr>
+                        <th>Émissions "Patrimoine et compétence" (<abbr title="tonne équivalent CO2">teq-CO2</abbr>)</th>
+                        <td>
+                            <?php if(!empty($diagnostic->EMIS_CO2_PC)): ?>
+                            <?php e($diagnostic->EMIS_CO2_PC) ?>
+                            <?php endif; ?>
+                        </td>
+                    </tr>  
                     <tr>
                         <td><strong>Bilan <abbr title="Gaz à effets de serre" class="initialism">GES</abbr> Territorial</strong></td>
                         <td>
